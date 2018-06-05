e6fac77ad7254f3cfa06c26f04e72e60b82bddd2
Michael Carlos <michaelpcarlos@gmail.com>
2018-06-05 16:21:32 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 const playlist = {kanyeWest: "Ghost Town", lilUziVert: "XO", danielCaesar: "Get You"};
 
 function updatePlaylist(playlist, artist, song) {
-  playlist.artist = song;
+  playlist[artist] = song;
   return playlist;
 }
